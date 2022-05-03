5dcdd1739a594af984e2b3ec9dbe4fc427595e48
hugosenari <hugosenari@gmail.com>
2022-05-03 01:31:15 -0300

    chore: re index fidget

--- 0_3_1/flake.nix
+++ 0_3_1/flake.nix
@@ -61,13 +61,13 @@
   inputs."opengl".inputs.nixpkgs.follows = "nixpkgs";
   inputs."opengl".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
-  inputs."nim-snappy".owner = "nim-nix-pkgs";
-  inputs."nim-snappy".ref   = "master";
-  inputs."nim-snappy".repo  = "nim-snappy";
-  inputs."nim-snappy".dir   = "master";
-  inputs."nim-snappy".type  = "github";
-  inputs."nim-snappy".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nim-snappy".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  inputs."snappy".owner = "nim-nix-pkgs";
+  inputs."snappy".ref   = "master";
+  inputs."snappy".repo  = "snappy";
+  inputs."snappy".dir   = "master";
+  inputs."snappy".type  = "github";
+  inputs."snappy".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."snappy".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   inputs."html5_canvas".owner = "nim-nix-pkgs";
   inputs."html5_canvas".ref   = "master";
